24590b6971731f083a233fd78cbdb07fcfbc9fad
agialif <agialif48@gmail.com>
2022-12-20 16:34:44 +0800

    update socket

--- app.js
+++ app.js
@@ -63,15 +63,14 @@ const io = socket(server, {
 
 global.onlineUsers = new Map();
 io.on("connection", (socket) => {
-  global.chatSocket = socket;
-  socket.on("add-user", (userId) => {
+  socket.on("online-user", (userId) => {
     onlineUsers.set(userId, socket.id)
   })
 
-  socket.on("send-msg", (data)=> {
-    const sendUserSocket = onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-receive", data.msg)
+  socket.on("msg-send", (data)=> {
+    const toUserSocket = onlineUsers.get(data.to);
+    if (toUserSocket) {
+      socket.to(toUserSocket).emit("rcv-msg", data.msg)
     }
   })
 })
